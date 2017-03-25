282813f0f2a89d3f2971272e7d0651909b9342c7
carllacan <carles.llacer@gmail.com>
2017-03-25 18:00:35 +0100

    Creates anki deck and gets definitions from OED

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import sqlite3
 import genanki
-import wx
-
+import requests
+import json
 
 if __name__ == '__main__':
 
     lang = 'en'#raw_input('What language do you want the words in? (available: en)')
     fname = 'vocab.db' #raw_input...
+    
     vocab = sqlite3.connect(fname)
     q = 'select WORDS.stem, LOOKUPS.usage, WORDS.timestamp from WORDS inner join LOOKUPS on WORDS.id=LOOKUPS.word_key where WORDS.lang=\'{}\''.format(lang)
     cursor = vocab.execute(q)
 
+    app_id = '91cff402'
+    app_key = 'edde3eeb5d8fe2ab96493642f535f580'
+    headers = {'app_id': app_id, 'app_key': app_key}
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'
+    
     deck = genanki.Deck(2059400110, "Kindle Words")
     model = genanki.Model(
         1607392319,
@@ -24,16 +30,29 @@ if __name__ == '__main__':
             {
                 'name': 'Card 1',
                 'qfmt': '{{Question}}',
-                'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}<hr id="usage"{{Usage}}>'
+                'afmt': '<b>{{FrontSide}}</b>: {{Answer}}<hr id="usage">{{Usage}}'
             }]
     )
     
+    added_words = []
     for w in cursor:
-        note = genanki.Note(
-            model=model,
-            fields=[w[0], 'descrp', w[1]]
-            )
-        deck.add_note(note)
+        
+        # Get OED definition
+        
+        r = requests.get(url  + lang + '/' + word_id.lower(), headers=headers) 
+        entries = r.json()['results'][0]['lexicalEntries'][0]['entries']
+        definition = entries[0]['senses'][0]['definitions'][0]
+        
+        stem = w[0]
+        usage = w[1]
+        
+        if stem not in added_words:
+            added_words.append(stem)
+            note = genanki.Note(
+                model=model,
+                fields=[stem, definition, usage]
+                )
+            deck.add_note(note)
 
     genanki.Package(deck).write_to_file('english.apkg')
     
